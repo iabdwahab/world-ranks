4f70fbba0aacad2edc943cc40a13537d13c05fdf
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-11-19 02:32:36 +0200

    Creating Header.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import Logo from './Logo';
+
+const Header = () => {
+  return (
+    <header className="py-4 border-b">
+      <div className="container">
+        <h1 className="font-bold md:text-2xl">
+          <Link href="/">
+            <Logo />
+          </Link>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
